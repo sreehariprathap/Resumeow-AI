1e05a4f62e09e2a9d8c0af262a8be871fc506691
Sreehari Prathap <sreehariprathap1996@gmail.com>
2025-05-16 18:00:41 -0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), viteStaticCopy({
+    targets: [
+      {
+        src: 'public/manifest.json',
+        dest: '.',
+      }
+    ],
+  }),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+})
